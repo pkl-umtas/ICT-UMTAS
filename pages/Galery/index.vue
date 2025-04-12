42c9c34c6e0428c6d4509b183a8207a818ac4fee
Dimasadhyaksa <143145756+Dimasadhyaksa@users.noreply.github.com>
2025-04-12 09:51:08 +0700

    Update index.vue

--- pages/Galery/index.vue
+++ pages/Galery/index.vue
@@ -1,13 +1,6 @@
 <template>
     <div class="container">
         <h4 class="mt-5">Galery</h4>
-        <div class="row mt-5 justify-content-center">
-            <div class="col-5">
-                <div class="card rounded-2 shadow">
-                    <img src="~/assets/img/tiga.jpg" class="card-img-top rounded-2" alt="img-artikel">
-                    <div class="card-body p-2 mt-1"><template>
-    <div class="container">
-        <h4 class="mt-5">Galery</h4>
 
         <div class="row mt-5 justify-content-center">
             <div class="col-5">
@@ -39,74 +32,6 @@
             <div class="col-5">
                 <div class="card rounded-2 shadow">
                     <img src="~/assets/img/aset3.jpg" class="card-img-top rounded-2" alt="img-artikel">
-                    <div class="card-body p-2 mt-1">
-                        <h6 class="text-center">Lapangan</h6>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</template>
-
-<style scoped>
-h4,
-h6 {
-    font-family: "Poppins", serif;
-}
-
-h4 {
-    margin-left: 95px;
-}
-
-.card {
-    background-color: transparent;
-    backdrop-filter: blur(20px);
-    border: 2px solid #ffff;
-}
-</style>
-                        <h6 class="text-center">Lapangan</h6>
-                    </div>
-                </div>
-            </div>
-            <div class="col-5">
-                <div class="card rounded-2 shadow">
-                    <img src="~/assets/img/tiga.jpg" class="card-img-top rounded-2" alt="img-artikel">
-                    <div class="card-body p-2 mt-1">
-                        <h6 class="text-center">Lapangan</h6>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row mt-5 justify-content-center">
-            <div class="col-5">
-                <div class="card rounded-2 shadow">
-                    <img src="~/assets/img/tiga.jpg" class="card-img-top rounded-2" alt="img-artikel">
-                    <div class="card-body p-2 mt-1">
-                        <h6 class="text-center">Lapangan</h6>
-                    </div>
-                </div>
-            </div>
-            <div class="col-5">
-                <div class="card rounded-2 shadow">
-                    <img src="~/assets/img/tiga.jpg" class="card-img-top rounded-2" alt="img-artikel">
-                    <div class="card-body p-2 mt-1">
-                        <h6 class="text-center">Lapangan</h6>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row mt-5 justify-content-center">
-            <div class="col-5">
-                <div class="card rounded-2 shadow">
-                    <img src="~/assets/img/tiga.jpg" class="card-img-top rounded-2" alt="img-artikel">
-                    <div class="card-body p-2 mt-1">
-                        <h6 class="text-center">Lapangan</h6>
-                    </div>
-                </div>
-            </div>
-            <div class="col-5">
-                <div class="card rounded-2 shadow">
-                    <img src="~/assets/img/tiga.jpg" class="card-img-top rounded-2" alt="img-artikel">
                     <div class="card-body p-2 mt-1">
                         <h6 class="text-center">Lapangan</h6>
                     </div>
